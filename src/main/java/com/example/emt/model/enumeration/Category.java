9ba0636e9b08ec35a2c9ccf986c484c2ef2bd577
DenicaKj <dkorvezir@gmail.com>
2023-03-27 23:43:18 +0200

    fix

--- src/main/java/com/example/emt/model/enumeration/Category.java
+++
@@ -1,5 +0,0 @@
-package com.example.emt.model.enumeration;
-
-public enum Category {
-    NOVEL, THRILER, HISTORY, FANTASY, BIOGRAPHY, CLASSICS, DRAMA
-}
--- src/main/java/com/example/emt/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.emt.repository;
-
-import com.example.emt.model.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends JpaRepository<Country,Long> {
-}
