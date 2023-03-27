5b2019b8768af87aee3b15ce0ecab693dab02e4e
DenicaKj <dkorvezir@gmail.com>
2023-03-27 02:05:20 +0200

    first commit

---
+++ src/test/java/com/example/emt/EmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
